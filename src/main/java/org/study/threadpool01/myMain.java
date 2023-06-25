8750b5427575a554c1be08487767418c92c3c64f
houlijia <wohoulijia@163.com>
2023-06-25 11:03:32 +0800

    新增线程池代码

--- src/main/java/org/study/threadpool01/myMain.java
+++ src/main/java/org/study/threadpool01/myMain.java
@@ -1,7 +1,6 @@
 package org.study.threadpool01;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @Author houlijia
@@ -10,12 +9,26 @@ import java.util.concurrent.Executors;
  **/
 public class myMain {
 
-    // public static ExecutorService newCachedThreadPool()             创建一个没有上限的线程池
-    // public static ExecutorService newFixedThreadPool(int nThread)   创建一个固定数量的线程池
+    // public static ExecutorService newCachedThreadPool()                创建一个没有上限的线程池
+    // public static ExecutorService newFixedThreadPool(int nThread)      创建一个固定数量的线程池
+    // ThreadPoolExecutor  threadpoolExecutor=new ThreadPoolExecutor（)    自定义线程池
     public static void main(String[] args) throws InterruptedException {
         //获取线程池对象
         //ExecutorService pool1 = Executors.newCachedThreadPool();
         ExecutorService pool1 = Executors.newFixedThreadPool(3);
+
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(
+                3, //核心线程池数量
+                6, //最大线程池数量
+                60,//空闲线程最大存活时间
+                TimeUnit.SECONDS,//存活时间单位
+                new ArrayBlockingQueue<Runnable>(3),  //任务队列
+                Executors.defaultThreadFactory(),//创建线程池工厂
+                new ThreadPoolExecutor.AbortPolicy()//默认拒绝策略
+        );
+
+        //pool.submit(new MyRunnable());   //提交任务
+
 
         //提交任务
       //  Thread.sleep(1000);
